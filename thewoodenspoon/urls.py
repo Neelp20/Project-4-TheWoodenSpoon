017934833f93f4c0daab43fcdab75c6efee5a89f
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-06-27 16:10:02 +0100

    fix for heroku error

--- thewoodenspoon/urls.py
+++ thewoodenspoon/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from home.views import home_app
+from home import views as home_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home_app, name='home'),
+    path('home/', home_views.home_app, name='home'),
 ]
