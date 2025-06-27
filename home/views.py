111c32c6d2817100ed7904d0e3cb54271d311987
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-06-27 15:42:47 +0100

    readies code for deploy

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home_app(request):
+    return HttpResponse("The wodden spoon home page!")
