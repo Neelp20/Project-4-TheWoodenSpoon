af35bbc09e911065fc49ea42cdc0219155d5df87
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-06-30 10:24:53 +0100

    modify login/logout/signup pages

--- thewoodenspoon/settings.py
+++ thewoodenspoon/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
+    'crispy_bootstrap5',
 
     # Apps
     'home',
@@ -66,6 +68,9 @@ MIDDLEWARE = [
     'allauth.account.middleware.AccountMiddleware',
 ]
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 ROOT_URLCONF = 'thewoodenspoon.urls'
 
 TEMPLATES = [
@@ -82,6 +87,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field'
+            ]
         },
     },
 ]
