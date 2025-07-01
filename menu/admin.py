4e8385ed51bfe889a36892f96df65eb90dfee796
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-07-01 14:38:49 +0100

    add hero image and welcome message

--- menu/admin.py
+++ menu/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import MenuItem
+# from .models import MenuItem
 
 # Register your models here.
 
 
-@admin.register(MenuItem)
-class MenuItemAdmin(admin.ModelAdmin):
-    """Lists field to display menu items"""
-    list_display = ('name', 'category', 'price')
-    list_filter = ('category',)
+# @admin.register(MenuItem)
+# class MenuItemAdmin(admin.ModelAdmin):
+#     """Lists field to display menu items"""
+#     list_display = ('name', 'category', 'price')
+#     list_filter = ('category',)
--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import MenuItem
 
 # Create your views here.
+
+
+class AddMenu(CreateView):
+    """Add menu view"""
+    template_name = 'menu/add_menu.html'
+    model = MenuItem
+    success_url = '/menu/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(AddMenu, self).form_valid(form)
+           
