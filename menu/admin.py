9fd9e1f540cd323bdc42e22c1661b076be4e65db
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-07-04 13:47:38 +0100

    deployment settings updated

--- menu/admin.py
+++ menu/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # from django_summernote.admin import SummernoteModelAdmin
-from .models import Menu, AllergyLabel, MenuItem
+from .models import Menu, MenuItem
 
 # Register your models here.
 
