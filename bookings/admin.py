f71c8b901c7e477c1cc50304f052493417e40cb9
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-07-08 13:39:51 +0100

    Create TableAdmin and BookingAdmin for admin page

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,2 +1,36 @@
 from django.contrib import admin
+from .models import Table, Booking
 
+# Register your models here.
+
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = (
+        "number",
+        "capacity"
+        )
+    list_filter = (
+        "number",
+        "capacity"
+    )
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "date",
+        "time",
+        "table",
+        "party_size"
+    )
+    list_filter = (
+        "date",
+        "table"
+    )
+    search_fields = (
+        "name",
+        "phone",
+        "date"
+    )
