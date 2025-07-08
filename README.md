9f0e7c8685c11930893713f26db2450bf6986175
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-07-08 13:31:09 +0100

    Create Table and Booking model for bookings

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 The Wooden Spoon is a fictional restaurant, which has a amazing setting and heartfelt hospitality promise a dining experience as memorable as the view.
 Our each dish crafted from the freshest ingredient sourced and we honour the flavour of the region in every bite.
 The live link can be found here: [Live Site - The Wooden Spoon](https://the-wooden-spoon-cfb803cde318.herokuapp.com/)
+
--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Table(models.Model):
+    """Model to create Tables"""
+    number = models.PositiveIntegerField(unique=True)
+    capacity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"Table {self.number} (Seats: {self.capacity})"
+ 
+
+class Booking(models.Model):
+    """Model to create bookings"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    table = models.ForeignKey(
+        Table, on_delete=models.CASCADE, related_name="booking")
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+
+    date = models.DateField()
+    time = models.TimeField()
+    party_size = models.PositiveIntegerField()
+    message = models.TextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """
+        unique_together will ensure to avoid double-bookings.
+        Order by date and then time
+        """
+        unique_together = ("table", "date", "time")
+        ordering = ("date", "time")
+
+    def __str__(self):
+        return f"{self.name} | {self.date} {self.time} | Table {self.table.number}"
