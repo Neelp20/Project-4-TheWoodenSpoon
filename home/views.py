8dfb08f6e8fcca2de0740cb00b7ee00c6748dba8
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-07-13 14:31:36 +0100

    Add docstring for home/views and remove unused import

--- home/views.py
+++ home/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 # Create your views here.
 
 
-# def home_app(request):
-#     return HttpResponse("The wodden spoon home page!")
-
-
 class Index(TemplateView):
+    """
+    View to render homepage
+    """
     template_name = 'home/index.html'
